9cfad607598af2ccbc9c91da2a088bb06083119b
shubhamxcode <shubh.varshneycode@gmail.com>
2024-02-21 01:27:02 +0530

    js

---
+++ 03_conversionoperation.js
@@ -0,0 +1,9 @@
+let score='723hfs'
+console.log(typeof(score))
+let valueinnumber=Number(score)//N is always capital
+console.log(typeof(valueinnumber))
+console.log(valueinnumber)//gives NaN means not a number
+//for null gives 0
+//false=>0 and true=>1
+//string=>NaN
+
