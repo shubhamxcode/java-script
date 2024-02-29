8b2c6550fa2eaf3434c5da3e7993bd370a928465
shubhamxcode <shubh.varshneycode@gmail.com>
2024-03-01 00:36:03 +0530

    arrays

---
+++ arrays.js
@@ -0,0 +1,37 @@
+const array=[2,3,332,23,43,true,'shubham']//0 se start hota hai
+console.log(array[0]) 
+//mutable,object type 
+//array copy operation
+//(1)shallow copy ( original copy) just like heap
+//(1)shallow copy ( nonoriginal copy) just like stack
+
+const array2= new Array(23,23,2)
+console.log(array2[2])
+
+const myaaray=[1,2,3,4,5,6]
+myaaray.push(33)
+myaaray.pop()//last value ko remove kar dega 
+console.log(myaaray)
+myaaray.unshift(9)//starting me 9 ko add kar dega 
+myaaray.shift()//starting me se 9 ko remove kar dega 
+console.log(myaaray)
+console.log(myaaray.includes(6))//gives value in bollean 
+console.log(myaaray.indexOf(3))// agar value hogi to bo position bata dega.. agar value exist nahi karti to -1 show karega 
+
+const newarrray=myaaray.join()
+console.log(newarrray)//output string me dega and datatype bhi string ban jayega 
+
+// slice and splice 
+
+const myaarays=[1,2,3,4,5,6]
+console.log('A', myaarays)
+const my1= myaarays.slice(1,3)//3 include nhi hoga 
+console.log(my1)
+console.log('B', myaarays)
+const my2=myaaray.splice(1,3)//3 include hoga 
+console.log('c',myaarays)
+console.log(my2)
+// interviw q( difference between slice and splice )
+// in slice  last does not increase range 
+// in splice splice part are remove 
+
--- date-time.js
+++ date-time.js
@@ -1,23 +1,33 @@
-// DATE(OBJECT TYPE)
-let mydate=new Date()//agar new hatayenge to indian time me dega time and date
-console.log(mydate)
-console.log(mydate.toString())
-console.log(mydate.toDateString())
-console.log(mydate.toLocaleTimeString())
+// // DATE(OBJECT TYPE)
+// let mydate=new Date()//agar new hatayenge to indian time me dega time and date
+// console.log(mydate)
+// console.log(mydate.toString())
+// console.log(mydate.toDateString())
+// console.log(mydate.toLocaleTimeString())
 
 
-// date and time declaration
-yy/mm//dd
+// // date and time declaration
+// yy/mm//dd
 
-let mynewdate= new Date(2024,2,2)//0 se start hoti hai month
-let newdate= new Date(2024,2,2,9,6)
-console.log(newdate.toDateString())
-console.log(newdate.toLocaleString())
-console.log(newdate.toLocaleString('en-in'))
-console.log(newdate.toLocaleDateString())
-mm//dd//yy
-let indd=new Date("3-6-2024")//01 se start hoti hai month
-console.log(indd.toLocaleString())
+// let mynewdate= new Date(2024,2,2)//0 se start hoti hai month
+// let newdate= new Date(2024,2,2,9,6)
+// console.log(newdate.toDateString())
+// console.log(newdate.toLocaleString())
+// console.log(newdate.toLocaleString('en-in'))
+// console.log(newdate.toLocaleDateString())
+// mm//dd//yy
+// let indd=new Date("3-6-2024")//01 se start hoti hai month
+// console.log(indd.toLocaleString()) 
 
-let inddd = new Date("2024-03-06:06:00");
-console.log(inddd.toLocaleString());
+// let inddd = new Date('2024,2,29');
+// console.log(inddd.getMonth()+1);//0 se satrt ho raha hai
+// console.log(inddd.getDay())//monday-0.......friday-4
+// console.log(inddd.getFullYear());
+
+
+let hh=new Date(2024,2,29);
+console.log(hh.toLocaleString('default',{
+    weekday: 'short',//gives fri
+    weekday: 'long'//gives friday
+//we can give many factors... date r=numeric me chahiye ya kisi or me chahiye..etc
+}))
