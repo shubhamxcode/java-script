36d8fedd4314d81d6a8a76d1b32ecd638849e863
shubhamxcode <shubh.varshneycode@gmail.com>
2024-04-11 17:37:59 +0530

    DOM

--- function.js
+++ function.js
@@ -1,62 +1,62 @@
-// function saymyname(){
-//     console.log('s')
-//     console.log('h')
-//     console.log('u')
-//     console.log('b')
-//     console.log('h')
-//     console.log('a')
-//     console.log('m')
-// }
-// saymyname()
+function saymyname(){
+    console.log('s')
+    console.log('h')
+    console.log('u')
+    console.log('b')
+    console.log('h')
+    console.log('a')
+    console.log('m')
+}
+saymyname()
 
-// function addtwonumbers(num1,num2){//num1 and num2 are paramiters))
-//     console.log(num1+num2)
+function addtwonumbers(num1,num2){//num1 and num2 are paramiters))
+    console.log(num1+num2)
 
-// }
-// addtwonumbers(3,null)//arguments
-// function addtwonumbers(num1,num2){//num1 and num2 are paramiters
-//     let result=num1+num2
-//     return result
-//     console.log('shubham')//it does not exicude couse the function return value already declare once
-// }
-// const returnvalue=addtwonumbers(4,3)
-// console.log(returnvalue)
-
-
-// function loginusermessage(username){
-//     if(username===undefined){
-//         console.log("please enter your username")
-//     }
-// }
-// console.log(loginusermessage())
-// // // undefine in if statement taken as false value and (!)are use to convert false value into true value and true value convert into false value 
-// function loginusermessage(username= "sam"){
-//     if(!username){
-//         console.log("please enter your username")
-//     }
-//     return `${username} just looged in `
-// }
-// console.log(loginusermessage())
+}
+addtwonumbers(3,null)//arguments
+function addtwonumbers(num1,num2){//num1 and num2 are paramiters
+    let result=num1+num2
+    return result
+    console.log('shubham')//it does not exicude couse the function return value already declare once
+}
+const returnvalue=addtwonumbers(4,3)
+console.log(returnvalue)
 
 
-// function calculatecartprice(var1,var2,...num){//... called rest operator
-//     return num
-// }
-// console.log(calculatecartprice(200,300,400))
+function loginusermessage(username){
+    if(username===undefined){
+        console.log("please enter your username")
+    }
+}
+console.log(loginusermessage())
+// // undefine in if statement taken as false value and (!)are use to convert false value into true value and true value convert into false value 
+function loginusermessage(username= "sam"){
+    if(!username){
+        console.log("please enter your username")
+    }
+    return `${username} just looged in `
+}
+console.log(loginusermessage())
 
-// const users={
-//     username:"shubham",
-//     price:3242
-// }
-// function army(system){
-//     console.log(`username is ${system.username} and price is ${system.price}`)
-// }
-// army(users)
-// // const handle=user
-// army({
-//     username:"shubh",
-//     price:800
-// })
+
+function calculatecartprice(var1,var2,...num){//... called rest operator
+    return num
+}
+console.log(calculatecartprice(200,300,400))
+
+const users={
+    username:"shubham",
+    price:3242
+}
+function army(system){
+    console.log(`username is ${system.username} and price is ${system.price}`)
+}
+army(users)
+// const handle=user
+army({
+    username:"shubh",
+    price:800
+})
 
 const myarray=[2143,2342,23]
 
