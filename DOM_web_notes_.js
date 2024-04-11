3a69d2328995d9895768470e5bca2d3e541fb4c9
shubhamxcode <shubh.varshneycode@gmail.com>
2024-04-11 12:03:25 +0530

    DOM

--- DOM_web_notes_.js
+++ DOM_web_notes_.js
@@ -1,2 +1,3 @@
 //innertext and textcontent diffrence is in innertext remove part are not visible but in textcontent remove part are visible and in innerhtml html tags will we shown
 
+//h.innertext='shubham'//aese hum texxt change kar sakte hai
