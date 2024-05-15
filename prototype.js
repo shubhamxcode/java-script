122ae0d88f645a2c08fa63a227bf2091f0b752e1
shubhamxcode <shubh.varshneycode@gmail.com>
2024-05-15 22:04:17 +0530

    prototype

---
+++ prototype.js
@@ -0,0 +1,45 @@
+const myhero=['superman','batman','thor']
+
+let herospower={
+    thor:'hammer',
+    spiderman:'sling',
+    getspider:function(){
+        console.log(`spidermaan power is ${this.spiderman}`);
+    }
+}
+let world="hello"
+Object.prototype.shubham=function(){
+    console.log(`spiderman my boy ${this.world}`);
+}
+shubham()
+herospower.shubham()
+Array.prototype.varshney=function(){
+    console.log('my array');
+}
+myhero.varshney()
+// herospower.varshney()//array pe power nahi hai ki bo object ko excess kar paye 
+
+
+const teacher=function(){
+    makevideo:true
+}
+const tasupport=function(){
+    makeassinment:true
+    // __proto__:teacher
+}
+
+teacher.__proto__=tasupport
+
+//modern syntak
+
+Object.setPrototypeOf(teacher,tasupport)
+
+
+
+let username='chaiorcode                ';
+
+String.prototype.truelength=function(){
+    // console.log(this.trim().length);
+    // console.log('shubham varshney');
+}
+username.truelength()
